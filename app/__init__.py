70252f95161d9cabd6926814ce9937e4d327b495
sagarsangwan <iamsagarsangwan@gmail.com>
2022-03-30 20:55:00 +0530

    initiating database connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,30 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def create_app():
-    from .posts import routes
     app = Flask(__name__)
-    app.register_blueprint(routes.posts_bp)
+    from .posts import routes as posts_routes
+    app.register_blueprint(posts_routes.posts_bp)
 
-    # from .weather import routes
-    # app.register
     return app
+
+
+app = create_app()
+
+
+def database_connection(app):
+    from flask_sqlalchemy import SQLAlchemy
+    from flask_migrate import Migrate
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy()
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    return db
+
+
+database_connection = database_connection(app)
--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,15 +1,20 @@
 from crypt import methods
-from flask import Blueprint, make_response, jsonify
-
-
-# Blueprint Configuration
-
+from flask import Blueprint, make_response, jsonify, request
 posts_bp = Blueprint(
     "posts_bp", __name__,
 )
 
 
-@posts_bp.route('/posts', methods=['GET'])
+@posts_bp.route('/posts', methods=['GET', 'POST'])
 def get_posts():
-    res = make_response(jsonify({'hii': 'this is posts page'}))
-    return res
+    if request.method == 'GET':
+        res = make_response(jsonify({'hii': 'this is posts page'}))
+        return res
+    elif request.method == 'POST':
+        details = request.args.to_dict()
+        messages = details.get("messages")
+        location = details.get("location")
+        print(messages, location)
+        res = make_response(
+            jsonify({'message': messages, 'location': location}), 200)
+        return res
